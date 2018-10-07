744dd8cdca69f85a6fbe27770d2f4277a267df77
Lam Phuoc Huy <38396079+NaginataAI@users.noreply.github.com>
2018-10-07 21:09:43 +0700

    Create Loops.py

---
+++ Easy/Loops.py
@@ -0,0 +1,10 @@
+""" Problem: Loops || Task: 
+Read an integer N. For all non-negative integers i < N, print i^2. See the sample for details.
+
+Created on Sun Oct  7 21:04:16 2018
+@author: nagiAI
+"""
+
+n = int(input())
+for i in range(n):
+    print(i**2)

a5741e2f66b05ef80b0a0c0b37a12a6bd111bb7c
Lam Phuoc Huy <38396079+NaginataAI@users.noreply.github.com>
2018-10-07 21:24:49 +0700

    Update Lists.py

--- Easy/Lists.py
+++ Easy/Lists.py
@@ -1,15 +1,15 @@
 """ Problem: Lists || Task: 
 Consider a list (list = []). You can perform the following commands:
 
-1. insert i e: Insert integer  at position .
+1. insert i e: Insert integer e at position i.
 2. print: Print the list.
-3. remove e: Delete the first occurrence of integer .
-4. append e: Insert integer  at the end of the list.
+3. remove e: Delete the first occurrence of integer e.
+4. append e: Insert integer e at the end of the list.
 5. sort: Sort the list.
 6. pop: Pop the last element from the list.
 7. reverse: Reverse the list.
 
-Initialize your list and read in the value of  followed by  lines of commands where each command will be of the types listed above. 
+Initialize your list and read in the value of n followed by n lines of commands where each command will be of the 7 types listed above. 
 Iterate through each command in order and perform the corresponding operation on your list.
 
 Created on Sun Oct  7 21:18:45 2018

9b8629023936e3d26d40ca884ef6f0889672467f
Lam Phuoc Huy <38396079+NaginataAI@users.noreply.github.com>
2018-10-10 11:00:48 +0700

    Create What's_Your_Name.py

---
+++ Easy/What's_Your_Name.py
@@ -0,0 +1,14 @@
+""" Problem: What's Your Name? || Task: 
+You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
+
+Hello firstname lastname! You just delved into python.
+
+Created on Wed Oct 10 10:41:59 2018
+@author: nagiAI
+"""
+
+def print_full_name(a, b):
+    print("Hello", a, b + "! You just delved into python.")
+    
+# print_full_name("Ross", "Taylor")
+

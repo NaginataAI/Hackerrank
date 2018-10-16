0c314e90010e3ab8fe3b898350f511b1c7e951fb
Lam Phuoc Huy <38396079+NaginataAI@users.noreply.github.com>
2018-10-16 16:05:12 +0700

    Create collections.Counter.py

---
+++ Easy/collections.Counter.py
@@ -0,0 +1,49 @@
+""" Problem: collections.Counter() || Task: 
+
+Raghu is a shoe shop owner. His shop has X number of shoes. 
+He has a list containing the size of each shoe he has in his shop. 
+There are N number of customers who are willing to pay xi amount of money only if they get the shoe of their desired size.
+Your task is to compute how much money Raghu earned.
+
+Created on Tue Oct 16 14:58:08 2018
+@author: nagiAI
+"""
+# Solution using list - Status: Accepted
+totalMoney = 0
+numShoe = int(input())
+listShoeSize = input().split(" ")
+numCustomer = int(input())
+
+for i in range(numCustomer):
+    size, money = input().split(" ")
+    size = size
+    money = int(money)
+    
+    if size in listShoeSize:
+        totalMoney += money
+        listShoeSize.remove(size)
+        
+print(totalMoney)
+
+# Solution using collections.Counter - Status: Accepted
+import collections
+
+totalMoney = 0
+numShoe = int(input())
+listShoeSize = collections.Counter(input().split(" "))
+numCustomer = int(input())
+    
+for i in range(numCustomer):
+    size, money = input().split(" ")
+    money = int(money)
+    
+    if listShoeSize[size] > 0:
+        listShoeSize[size] -= 1
+        totalMoney += money
+        
+print(totalMoney)
+
+
+    
+
+
